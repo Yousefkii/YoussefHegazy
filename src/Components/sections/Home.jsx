5a461431e1f55cd4b3b8898b08d23726542b9fd1
Yousefkii <youssefhegaze908@gmail.com>
2025-03-22 15:20:00 +0100

    Creating and applying the RevelOnScroll component

--- src/Components/sections/Home.jsx
+++ src/Components/sections/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import RevealOnScroll from '../RevealOnScroll'
 
 const Home = () => {
   return (
     <section id='home
     ' className='min-h-screen flex items-center justify-center relative'> 
+    <RevealOnScroll>
   <div className='text-center z-10 px-4'>
     <h1 className='text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent '>
       Hi, I'm Youssef Hegazy
@@ -21,6 +23,7 @@ const Home = () => {
 </a>
 </div>
   </div>
+  </RevealOnScroll>
     </section>
   )
 }
--- src/Components/sections/Projects.jsx
+++ src/Components/sections/Projects.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import RevealOnScroll from '../RevealOnScroll'
 
 const Projects = () => {
   return (
     <section id='projects' className='min-h-screen flex items-center justify-center py-20'>
+      <RevealOnScroll>
 <div className='max-w-5xl mx-auto px-4'> 
  <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'>
  Featured projects
@@ -36,7 +38,7 @@ const Projects = () => {
     A responsive user-friendly AI website Created with reuseable componentes like BUttons and Sections
     </p>
     <div className='pt-3'>
-        {['React','JavaScript','TailwindCSS'].map((tech,key)=>(
+        {['React','JavaScript','CSS','Gsap'].map((tech,key)=>(
           <span className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition'key={key}>
           {tech}
            </span>
@@ -50,6 +52,7 @@ const Projects = () => {
 </div>
  </div>
 </div>
+</RevealOnScroll>
     </section>
   )
 }
