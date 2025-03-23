dac5b49475177a9771fe3caafbbc3f285cc55a98
Yousefkii <youssefhegaze908@gmail.com>
2025-03-23 11:26:37 +0100

    modifying the linking inside the page

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -17,9 +17,9 @@ const Navbar = ({menuOpen, setMenuOpen}) => {
            &#9776;
          </div>
          <div className="hidden md:flex items-center space-x-8">
-           <a href="#Home" className="text-gray-300 hover:text-white transition-colors"> Home </a>
-           <a href="#About" className="text-gray-300 hover:text-white transition-colors"> About </a>
-           <a href="#Projects" className="text-gray-300 hover:text-white transition-colors"> Projects </a>
+           <a href="#home" className="text-gray-300 hover:text-white transition-colors"> Home </a>
+           <a href="#about" className="text-gray-300 hover:text-white transition-colors"> About </a>
+           <a href="#projects" className="text-gray-300 hover:text-white transition-colors"> Projects </a>
            <a href="#contact" className="text-gray-300 hover:text-white transition-colors"> contact </a>
          </div>
       </div>
--- src/Components/sections/Home.jsx
+++ src/Components/sections/Home.jsx
@@ -15,10 +15,10 @@ const Home = () => {
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt adipisci qui veniam animi quia esse cupiditate, enim tempora explicabo sequi similique. Perferendis ducimus consequuntur vero repellat cupiditate quis inventore deleniti.
 </p></span>
 <div className='flex justify-center space-x-4'>
-<a href='#Projects' className='bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]'>
+<a href='#projects' className='bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]'>
     View Projects
 </a>
-<a href='#Contact' className='border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10'>
+<a href='#contact' className='border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10'>
     Contact me
 </a>
 </div>
